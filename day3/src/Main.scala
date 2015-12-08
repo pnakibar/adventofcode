57c5601b2fe21a65148e0bdea125c197a0ac3966
pnakibar <pedro.nakibar@gxdevel.com.br>
2015-12-08 00:58:51 -0200

    day 3 completed!

--- day3/src/Main.scala
+++ day3/src/Main.scala
@@ -18,21 +18,49 @@ object Main extends App {
       case '<' => Position(actualPosition.x - 1, actualPosition.y)
     }
 
+  def doDeliver(actualPosition: Position, deliveries: List[Position]) =
+    deliveries contains actualPosition match {
+      case true => deliveries
+      case false => actualPosition :: deliveries
+    }
+
   def deliver(inp: String): List[Position] = {
-    def loop(inp: String, housesDelivered: List[Position], actualPosition: Position): List[Position] = {
-      val actualList = housesDelivered contains actualPosition match {
-        case true => housesDelivered
-        case false => actualPosition :: housesDelivered
+    def loop(inp: String, housesDelivered: List[Position], realSanta: Position): List[Position] = {
+      val actualList = doDeliver(realSanta, housesDelivered)
+
+      val newRealSantaPosition = Try {
+        nextPosition(inp, realSanta)
       }
 
-      Try {nextPosition(inp, actualPosition)} match {
-        case Success(nextHouse) => loop(inp tail, actualList, nextHouse)
+      newRealSantaPosition match {
+        case Success(nextHouse) => loop(inp.tail.tail, actualList, nextHouse)
         case Failure(_) => actualList
       }
     }
 
-    loop(inp, Nil, Position(0,0))
+    loop(inp, Nil, Position(0, 0))
+  }
 
+  def deliverRobot(inp: String): List[Position] = {
+    def loop(inp: String, housesDelivered: List[Position], robotSanta: Position, realSanta: Position): List[Position] = {
+      val actualList = doDeliver(robotSanta, doDeliver(realSanta, housesDelivered))
+
+      val newRealSantaPosition = Try {
+        nextPosition(inp, realSanta)
+      }
+      val newRobotSantaPosition = Try {
+        nextPosition(inp.tail, robotSanta)
+      }
+
+
+      (newRealSantaPosition, newRobotSantaPosition) match {
+        case (Success(realNextHouse), Success(robotNextHouse)) => loop(inp.tail.tail, actualList, robotNextHouse, realNextHouse)
+        case (Success(realNextHouse), Failure(_)) => loop(inp.tail.tail, actualList, robotSanta, realNextHouse)
+        case (Failure(_), Failure(_)) => actualList
+      }
+    }
+
+    loop(inp, Nil, Position(0, 0), Position(0, 0))
   }
 
   val houses = scala.io.Source.fromFile("input.txt").mkString
